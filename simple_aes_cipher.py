4bfc1b11265b660b7a9d6df0100a1044adcb8dad
teitei-tk <teitei.tk@gmail.com>
2016-07-05 11:24:58 -0700

    add library code and dependency library

---
+++ simple_aes_cipher.py
@@ -0,0 +1,29 @@
+import base64
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+    def __init__(self, key, block_size=32):
+        self.bs = block_size
+        if len(key) >= len(str(block_size)):
+            self.key = key[:block_size]
+        else:
+            self.key = self._pad(key)
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw)).decode('utf-8')
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    def _unpad(self, s):
+        return s[:-ord(s[len(s)-1:])]
