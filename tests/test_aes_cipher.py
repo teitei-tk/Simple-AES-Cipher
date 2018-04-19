bbeb1339851bc78f5099c4268ccf2c41b364947e
l3msh0 <L3msh0@gmail.com>
2018-04-20 01:08:46 +0900

    Accept key length other than 128bit

--- tests/test_aes_cipher.py
+++ tests/test_aes_cipher.py
@@ -15,23 +15,35 @@ class TestAESCipher(unittest.TestCase):
     def setUp(self):
         pass_phrase = self._choice_randome_string()
         self.secret_key = generate_secret_key(pass_phrase)
-        self.cipher = AESCipher(self.secret_key)
 
     def _choice_randome_string(self):
         return ''.join([random.choice(string.ascii_letters + string.digits)])
 
-    def test_encrypt(self):
+    def _get_cipher(self, key_length=128):
+        return AESCipher(self.secret_key, block_size=key_length)
+
+    def test_encrypt_128(self):
         text = self._choice_randome_string()
-        encrypt_text = self.cipher.encrypt(text)
+        encrypt_text = self._get_cipher(key_length=128).encrypt(text)
+        self.assertNotEqual(text, encrypt_text)
+
+    def test_encrypt_192(self):
+        text = self._choice_randome_string()
+        encrypt_text = self._get_cipher(key_length=192).encrypt(text)
+        self.assertNotEqual(text, encrypt_text)
+
+    def test_encrypt_256(self):
+        text = self._choice_randome_string()
+        encrypt_text = self._get_cipher(key_length=256).encrypt(text)
         self.assertNotEqual(text, encrypt_text)
 
     def test_decrypt(self):
         text = self._choice_randome_string()
 
-        encrypt_text = self.cipher.encrypt(text)
+        encrypt_text = self._get_cipher().encrypt(text)
         self.assertNotEqual(text, encrypt_text)
 
-        decrypt_text = self.cipher.decrypt(encrypt_text)
+        decrypt_text = self._get_cipher().decrypt(encrypt_text)
         self.assertNotEqual(encrypt_text, decrypt_text)
         self.assertEqual(text, decrypt_text)
 
